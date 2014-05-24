6155b29ec4e4171709ec9144ae89af2bfa4c63d2
Andrew Cann <himthingwatsit@gmail.com>
2014-05-24 14:15:50 +0700

    Fix build. Replace some instances of ~str with StrBuf

--- examples/ex_2.rs
+++ examples/ex_2.rs
@@ -43,7 +43,7 @@ fn main()
     /* Enable attributes and output message. */
     printw("\nKey pressed: ");
     attron(A_BOLD() | A_BLINK());
-    printw(format!("{}\n", char::from_u32(ch as u32).expect("Invalid char")));
+    printw(format!("{}\n", char::from_u32(ch as u32).expect("Invalid char")).as_slice());
     attroff(A_BOLD() | A_BLINK());
   }
 
