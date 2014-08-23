65838f5c35288e359cb152fda96eb259c15cb99e
Michael Gehring <mg@ebfe.org>
2014-08-23 02:43:18 +0200

    Add repr(C) to types used in C calls

--- src/ll.rs
+++ src/ll.rs
@@ -31,9 +31,11 @@ pub type FILE_p = *mut FILE;
 pub type va_list = *mut u8;
 
 /* Custom Types. */
+#[repr(C)]
 pub struct WINDOW_impl;
+#[repr(C)]
 pub struct SCREEN_impl;
-
+#[repr(C)]
 pub struct MEVENT { id: c_short, x: c_int, y: c_int, z: c_int, bstate: mmask_t}
 
 macro_rules! define_sharedffi(
